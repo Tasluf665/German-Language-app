80f15b06431aeaef5f76c87b3f57591cf947d60e
Tasluf665 <78264660+Tasluf665@users.noreply.github.com>
2021-10-06 20:11:58 +0600

    Full Data

--- Data/WordList.js
+++ Data/WordList.js
@@ -64,4 +64,189 @@ export const WordList = [
       name: "Essen",
       meaning: "Eat",
    },
+   {
+      id: "13",
+      name: "Fahren",
+      meaning: "Drive",
+   },
+   {
+      id: "14",
+      name: "Finden",
+      meaning: "Find",
+   },
+   {
+      id: "15",
+      name: "Fliegen",
+      meaning: "Fly",
+   },
+   {
+      id: "16",
+      name: "Fragen",
+      meaning: "Ask",
+   },
+   {
+      id: "17",
+      name: "Geben",
+      meaning: "Give",
+   },
+   {
+      id: "18",
+      name: "Gehen",
+      meaning: "Go",
+   },
+   {
+      id: "19",
+      name: "Helfen",
+      meaning: "Help",
+   },
+   {
+      id: "20",
+      name: "Hören",
+      meaning: "Listen",
+   },
+   {
+      id: "21",
+      name: "Kaufen",
+      meaning: "Buy",
+   },
+   {
+      id: "22",
+      name: "Kommen",
+      meaning: "Come",
+   },
+   {
+      id: "23",
+      name: "Kosten",
+      meaning: "Cost",
+   },
+   {
+      id: "24",
+      name: "Lesen",
+      meaning: "Read",
+   },
+   {
+      id: "25",
+      name: "Lieben",
+      meaning: "Love",
+   },
+   {
+      id: "26",
+      name: "Machen",
+      meaning: "Make",
+   },
+   {
+      id: "27",
+      name: "Nehmen",
+      meaning: "Take",
+   },
+   {
+      id: "28",
+      name: "öffnen",
+      meaning: "Open",
+   },
+   {
+      id: "29",
+      name: "Probieren",
+      meaning: "Try",
+   },
+   {
+      id: "30",
+      name: "Regnen",
+      meaning: "Rain",
+   },
+   {
+      id: "31",
+      name: "Reisen",
+      meaning: "Travel",
+   },
+   {
+      id: "32",
+      name: "Sagen",
+      meaning: "Tell",
+   },
+   {
+      id: "33",
+      name: "Schlafen",
+      meaning: "Sleep",
+   },
+   {
+      id: "34",
+      name: "Schmecken",
+      meaning: "Taste",
+   },
+   {
+      id: "35",
+      name: "Schreiben",
+      meaning: "Write",
+   },
+   {
+      id: "36",
+      name: "Schwimmen",
+      meaning: "Swim",
+   },
+   {
+      id: "37",
+      name: "Sehen",
+      meaning: "See",
+   },
+   {
+      id: "38",
+      name: "Senden",
+      meaning: "Send",
+   },
+   {
+      id: "39",
+      name: "Setzen",
+      meaning: "Set",
+   },
+   {
+      id: "40",
+      name: "Singen",
+      meaning: "Sing",
+   },
+   {
+      id: "41",
+      name: "Spielen",
+      meaning: "Act",
+   },
+   {
+      id: "42",
+      name: "Sprechen",
+      meaning: "Speak",
+   },
+   {
+      id: "43",
+      name: "Suchen",
+      meaning: "Search",
+   },
+   {
+      id: "44",
+      name: "Trinken",
+      meaning: "Drink",
+   },
+   {
+      id: "45",
+      name: "Vergessen",
+      meaning: "Forget",
+   },
+   {
+      id: "46",
+      name: "Verstehen",
+      meaning: "Mean",
+   },
+   {
+      id: "47",
+      name: "Warten",
+      meaning: "Wait",
+   },
+   {
+      id: "48",
+      name: "Wohnen",
+      meaning: "Live",
+   },
+   {
+      id: "49",
+      name: "Zeigen",
+      meaning: "Show",
+   },
 ];
